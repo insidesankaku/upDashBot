decdec2ac5586135b10be1a30bc436accfebd8b1
insidesankaku <borysborysenko@cloud.upwork.com>
2023-06-27 15:32:58 +0200

    add vue project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+// import TheWelcome from './components/TheWelcome.vue'
+import chatList from "./components/chatList.vue";
+
+</script>
+
+<template>
+  <main>
+    <chatList />
+  </main>
+</template>
+
+<style scoped>
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
